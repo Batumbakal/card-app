c0e0cff6b35691dea4521d8af1339e37f388287e
Batumbakal <arisgamaas7@gmail.com>
2019-09-26 13:52:51 +0700

    Updates

---
+++ precache-manifest.6f31527c92e89944cdc91a119e058378.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c27d82a7bd33f086c3787a2f27a4d8",
+    "url": "/card-app/index.html"
+  },
+  {
+    "revision": "dcb3b9985f64bcdf3a21",
+    "url": "/card-app/static/css/main.180b46e9.chunk.css"
+  },
+  {
+    "revision": "9c294afb0f4dd1820f10",
+    "url": "/card-app/static/js/2.7db0e45e.chunk.js"
+  },
+  {
+    "revision": "dcb3b9985f64bcdf3a21",
+    "url": "/card-app/static/js/main.4c3d2a55.chunk.js"
+  },
+  {
+    "revision": "20bb55ee7e1c999f9274",
+    "url": "/card-app/static/js/runtime-main.c5084dcb.js"
+  }
+]);
